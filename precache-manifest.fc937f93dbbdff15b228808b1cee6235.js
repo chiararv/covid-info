14c9cbb5a93db250e8c93aac6a2b82076c0488b1
Chiara Rosa Velazco <chiara.rosavelazco@gmail.com>
2020-08-11 15:24:14 -0300

    Updates

---
+++ precache-manifest.fc937f93dbbdff15b228808b1cee6235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b107bfc7bf69703484c97f76630799b",
+    "url": "/covid-info/index.html"
+  },
+  {
+    "revision": "57ef69dbb265ff811ff4",
+    "url": "/covid-info/static/css/main.667d69dc.chunk.css"
+  },
+  {
+    "revision": "bbbd242c5ba87f616975",
+    "url": "/covid-info/static/js/2.4db15991.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid-info/static/js/2.4db15991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ef69dbb265ff811ff4",
+    "url": "/covid-info/static/js/main.90b349de.chunk.js"
+  },
+  {
+    "revision": "7c77e78f2ce2b5064854",
+    "url": "/covid-info/static/js/runtime-main.ee2e9a00.js"
+  },
+  {
+    "revision": "d9e27eed339ac380aaa6a31672b3624d",
+    "url": "/covid-info/static/media/corona.d9e27eed.png"
+  }
+]);
